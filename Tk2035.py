ace3eb612d54a1fd908d7bca58bce5c7d09d27ae
Cat <3494295627@qq.com>
2021-12-10 07:21:07 +0800

    Update Tk2035.py

--- Tk2035.py
+++ Tk2035.py
@@ -1,4 +1,21 @@
-import tkinter
+from tkinter import *
 from time import sleep
+
+class MAIN:
+    def __init__(this):
+        #One step from arguments
+        this.ROOT = Tk()
+        this.MAIN = Canvas(ROOT, bg='#222222', width = 400, height = 400)
+        this.MAIN.pack()
+
+    def loop(this):
+        #The main loop
+        this.loop_content()
+        sleep(0.05)
+
+    def loop_content(this):
+        pass
+
+
 
 #This program comes with a strange name... It doesn't matter.
