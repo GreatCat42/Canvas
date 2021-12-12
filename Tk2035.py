7b8b18fa7d29b3c797103e4584df44af29438403
Cat <3494295627@qq.com>
2021-12-12 12:08:12 +0800

    Finish Tk2035.py and 2035 Demo

--- Tk2035.py
+++ Tk2035.py
@@ -5,17 +5,24 @@ class MAIN:
     def __init__(this):
         #One step from arguments
         this.ROOT = Tk()
-        this.MAIN = Canvas(ROOT, bg='#222222', width = 400, height = 400)
+        this.MAIN = Canvas(this.ROOT, bg='#222222', width = 400, height = 400)
         this.MAIN.pack()
 
     def loop(this):
         #The main loop
-        this.loop_content()
-        sleep(0.05)
+        while True:
+            this.loop_content()
+            this.ROOT.update()
+            sleep(0.05)
 
-    def loop_content(this):
-        pass
+    def line(this,x1,y1,x2,y2):
+        #Draws a line
+        this.MAIN.create_line(x1,y1,x2,y2,fill='#22cc22')
 
+    def delete(this,target):
+        this.MAIN.delete(target)
 
+    def clear(this):
+        this.MAIN.delete(ALL)
 
 #This program comes with a strange name... It doesn't matter.
